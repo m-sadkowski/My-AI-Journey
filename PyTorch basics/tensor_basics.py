1c9187bcbd2d9364c223405a6d417f9a4878f300
m-sadkowski <msadkowski000@gmail.com>
2025-03-15 00:43:28 +0100

    Update tensor_basics.py

--- PyTorch basics/tensor_basics.py
+++ PyTorch basics/tensor_basics.py
@@ -6,10 +6,11 @@ source of knowledge: https://www.youtube.com/watch?v=d2dS8kLQHco&ab_channel=Robe
 
 import torch
 
+# Check if CUDA (GPU support) is available, and set the device accordingly
 if torch.cuda.is_available():
-    device = 'cuda'
+    device = 'cuda' 
 else:
-    device = 'cpu'
+    device = 'cpu'  
 
 tensor_1 = torch.tensor([[2, 4, 5],
                          [1, 1, 1],
@@ -20,9 +21,13 @@ tensor_2 = torch.tensor([[2, 4, 5],
                          [3, 1, 0]])
 
 tensor_1_move_to_gpu_or_cpu = tensor_1.to(device=device)
+
 tensor_2_move_to_gpu_or_cpu = tensor_2.to(device=device)
 
 tensor_sum = tensor_1_move_to_gpu_or_cpu + tensor_2_move_to_gpu_or_cpu
 
-print(tensor_sum.ndim)
-print(tensor_sum.shape)
+# Print the number of dimensions (ndim) of the resulting tensor
+print(tensor_sum.ndim)  # Output: 2 (since it's a 2D tensor)
+
+# Print the shape of the resulting tensor
+print(tensor_sum.shape)  # Output: torch.Size([3, 3]) (3 rows and 3 columns)
